08ba9ccbfc3eed525ae2211e8924caddf1d500ee
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-03-31 20:46:36 +0700

    Lesson 3

---
+++ Lesson4/data.js
@@ -0,0 +1,8 @@
+const students = [
+    {id: 1, name: 'Xuân Tú', age: 18},
+    {id: 2, name: 'Minh Nhật', age: 13},
+    {id: 3, name: 'Gia Bảo', age: 13},
+    {id: 4, name: 'Bảo Lâm', age: 13},
+    {id: 5, name: 'Khải Hoàng', age: 13},
+    {id: 6, name: 'Quang Huy', age: 14}
+]
--- Lesson4/script.js
+++ Lesson4/script.js
@@ -33,3 +33,46 @@ form.addEventListener('submit', function(event) {
     message.textContent = 'Đăng ký thành công!';
     message.style.color = 'green';
 });
+
+// Bài 2: Lấy data từ file js
+function displayStudents() {
+    // Dùng DOM lấy container
+    const container = document.getElementById('studentList');
+
+    // Duyệt danh sách students
+    students.forEach(student => {
+        // Tạo thẻ li cho từng student
+        const li = document.createElement('li');
+        li.textContent = `${student.name} - ${student.age} tuổi`;
+        // Thêm thẻ li vào container
+        container.appendChild(li);
+    });
+}
+    // Gọi hàm thực hiện
+displayStudents();
+
+// Bài 3: Lấy data từ file JSON
+fetch('./data.json')
+    // chuyển đổi dữ liệu thành đối tượng JavaScript (JSON)
+    .then(response => response.json())
+    // xử lý dữ liệu sau khi đã chuyển đổi
+    .then(data => {
+        // Dùng DOM lấy container
+        const container = document.getElementById('productList');
+
+        // Duyệt danh sách products
+        data.forEach(product => {
+            // Tạo thẻ div có class "product-card" cho từng sản phẩm
+            const productCard = document.createElement('div');
+            productCard.className = 'product-card';
+
+            // Thêm dữ liệu cho card
+            productCard.innerHTML = `
+                <img src="${product.image}" alt="${product.name}">
+                <h3>${product.name}</h3>
+                <p>Giá: ${product.price} VND</p>
+            `;
+            // Thêm card vào container
+            container.appendChild(productCard);
+        });
+    });
